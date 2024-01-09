72e88b998f4efbf6107f5fad7119ab66d374e9c8
anada <anada@beingcorp.co.jp>
2024-01-09 11:15:49 +0900

    インスタンス変数を使わない形で修正し、メソッドの引数と返り値を活用

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -5,35 +5,35 @@
 MAX_ROW = 3
 
 def file_import
-  @max_length = 0
-  @array_files = []
+  max_length = 0
+  array_files = []
   Dir.foreach('.') do |item|
     next if item.start_with?('.')
 
-    @array_files << item
-    @max_length = item.length if @max_length < item.length
+    array_files << item
+    max_length = item.length if max_length < item.length
   end
+  [array_files, max_length]
 end
 
-def file_convert
-  file_import
-  array_row = @array_files.size / MAX_ROW
-  remainder = @array_files.size % MAX_ROW
+def file_convert(array_files)
+  array_row = array_files.size / MAX_ROW
+  remainder = array_files.size % MAX_ROW
   array_row += 1 unless remainder.zero?
   array_nil = 0
   array_nil = MAX_ROW - remainder unless remainder.zero?
   yoko_files = []
-  @array_files.each_slice(array_row) { |file| yoko_files << file }
+  array_files.each_slice(array_row) { |file| yoko_files << file }
   array_nil.times { yoko_files[MAX_ROW - 1] << 'dummy' }
-  @tate_files = yoko_files.transpose
-  array_nil.times { |i| @tate_files[array_row - 1 - i].pop }
+  tate_files = yoko_files.transpose
+  array_nil.times { |i| tate_files[array_row - 1 - i].pop }
+  tate_files
 end
 
-def file_export
-  file_convert
-  @tate_files.each do |files|
+def file_export(tate_files, max_length)
+  tate_files.each do |files|
     files.each do |file_name|
-      blank_length = @max_length - file_name.length + 2
+      blank_length = max_length - file_name.length + 2
       print file_name
       blank_length.times { print ' ' }
     end
@@ -41,4 +41,6 @@ def file_export
   end
 end
 
-file_export
+array_files, max_length = file_import
+tate_files = file_convert(array_files)
+file_export(tate_files, max_length)
