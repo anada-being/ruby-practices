f4e3f96304b9a218ec241ed70e7057f77a40801a
anada <anada@beingcorp.co.jp>
2024-01-11 11:10:51 +0900

    ファイル名の取得と最長のファイル名の長さの取得を簡潔にした。命名の変更などリファクタリングした。

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -5,14 +5,8 @@
 MAX_ROW = 3
 
 def file_import
-  max_length = 0
-  array_files = []
-  Dir.foreach('.') do |item|
-    next if item.start_with?('.')
-
-    array_files << item
-    max_length = item.length if max_length < item.length
-  end
+  array_files = Dir.foreach('.').to_a.filter { |file| !file.start_with?('.') }
+  max_length = array_files.map(&:length).max
   [array_files, max_length]
 end
 
@@ -20,22 +14,19 @@ def file_convert(array_files)
   array_row = array_files.size / MAX_ROW
   remainder = array_files.size % MAX_ROW
   array_row += 1 unless remainder.zero?
-  array_nil = 0
-  array_nil = MAX_ROW - remainder unless remainder.zero?
-  yoko_files = []
-  array_files.each_slice(array_row) { |file| yoko_files << file }
-  array_nil.times { yoko_files[MAX_ROW - 1] << 'dummy' }
+  dummy_number = remainder.zero? ? 0 : (MAX_ROW - remainder)
+  yoko_files = array_files.each_slice(array_row).to_a
+  dummy_number.times { yoko_files[MAX_ROW - 1] << 'dummy' }
   tate_files = yoko_files.transpose
-  array_nil.times { |i| tate_files[array_row - 1 - i].pop }
+  dummy_number.times { |i| tate_files[array_row - 1 - i].pop }
   tate_files
 end
 
 def file_export(tate_files, max_length)
   tate_files.each do |files|
     files.each do |file_name|
-      blank_length = max_length - file_name.length + 2
       print file_name
-      blank_length.times { print ' ' }
+      (max_length - file_name.length + 2).times { print ' ' }
     end
     print "\n"
   end
