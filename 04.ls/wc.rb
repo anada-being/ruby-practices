3ffcace3fc1fa1da5eed957f7fd45ee97cd509ca
anada <anada@beingcorp.co.jp>
2024-01-25 15:48:13 +0900

    rubocopを実行して、修正

--- 04.ls/wc.rb
+++ 04.ls/wc.rb
@@ -2,21 +2,30 @@
 
 # frozen_string_literal: true
 
+require 'optparse'
+
 def line_count(files)
   total = 0
   files.map do |f|
+    next unless FileTest.directory?(f) || FileTest.file?(f)
+
     if FileTest.directory?(f)
       puts "wc: #{f}: Is a directory"
-      printf("%7d",'0')
+      printf('%7d', '0')
     else
       line = File.read(f).count("\n", "/[^\n]\z/")
-      printf("%7s","#{line}")
+      printf('%7d', line)
       total += line
     end
     puts " #{f}"
   end
-  printf("%7s","#{total}")
-  puts " total"
+  printf('%7d', total)
+  puts ' total'
 end
 
-line_count(ARGV)
+opt = OptionParser.new
+params = {}
+opt.on('-l') { |v| params[:l] = v }
+opt.parse(ARGV)
+
+line_count(ARGV) if params[:l]
