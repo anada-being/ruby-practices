acb7de3ebfe578aa4947ada5ca2b3b4a8020ccc9
anada <anada@beingcorp.co.jp>
2024-01-30 18:05:16 +0900

    オプションなしの時の実装

--- 04.ls/wc.rb
+++ 04.ls/wc.rb
@@ -5,27 +5,59 @@
 require 'optparse'
 
 def line_count(files)
-  total = 0
   files.map do |f|
+    next unless FileTest.directory?(f) || FileTest.file?(f)
+    next if FileTest.directory?(f)
+    File.read(f).count("\n", "/[^\n]\z/")
+  end
+end
+
+def word_count(files)
+  files.map do |f|
+    next unless FileTest.directory?(f) || FileTest.file?(f)
+    next if FileTest.directory?(f)
+    File.read(f).split.size
+  end
+end
+
+def bytes_count(files)
+  files.map do |f|
+    next unless FileTest.directory?(f) || FileTest.file?(f)
+    next if FileTest.directory?(f)
+
+    File.stat(f).size
+  end
+end
+
+def output(*arr)
+  totals = [0,0,0]
+  ARGV.each_with_index do |f, i|
     next unless FileTest.directory?(f) || FileTest.file?(f)
 
     if FileTest.directory?(f)
       puts "wc: #{f}: Is a directory"
-      printf('%7d', '0')
+      3.times { printf('%8s', '0 ') }
     else
-      line = File.read(f).count("\n", "/[^\n]\z/")
-      printf('%7d', line)
-      total += line
+      arr.each_with_index do |data, num|
+        printf('%8s', "#{data[i]} ")
+        totals[num] += data[i]
+      end
     end
-    puts " #{f}"
+    puts "#{f}"
   end
-  printf('%7d', total)
+  totals.each { |total| printf('%8s', "#{total} ") }
   puts ' total'
 end
 
 opt = OptionParser.new
 params = {}
 opt.on('-l') { |v| params[:l] = v }
-opt.parse(ARGV)
+opt.on('-w') { |v| params[:w] = v }
+opt.on('-c') { |v| params[:c] = v }
 
-line_count(ARGV) if params[:l]
+lines = line_count(ARGV)
+words = word_count(ARGV)
+bytes = bytes_count(ARGV)
+arr = [lines, words, bytes]
+
+output(*arr)
