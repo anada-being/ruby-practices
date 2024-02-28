e4eb1d98c1d8e4ba3dbcfc02ae7c8594df4735af
anada <anada@beingcorp.co.jp>
2024-02-28 10:17:11 +0900

    hashの形式を統一、命名変更 receive_command_argument→parse_options, count_stdin→calculate_stdin, opt_params→options, inputs→input, count→property, file_name→path, padding_left→width

--- 04.ls/wc.rb
+++ 04.ls/wc.rb
@@ -5,42 +5,44 @@
 require 'optparse'
 
 def main
-  options, paths = receive_command_argument
-  paths.empty? ? output(count_stdin, options) : output(calculate_file_text(paths), options)
+  options, paths = parse_options
+  paths.empty? ? output(calculate_stdin, options) : output(calculate_file_text(paths), options)
 end
 
-def receive_command_argument
-  opt_params = { l: false, w: false, c: false }
+def parse_options
+  options = { l: false, w: false, c: false }
   opt = OptionParser.new
 
-  opt.on('-l') { |v| opt_params[:l] = v }
-  opt.on('-w') { |v| opt_params[:w] = v }
-  opt.on('-c') { |v| opt_params[:c] = v }
-  argv = opt.parse(ARGV)
-  [opt_params, argv]
+  opt.on('-l') { |v| options[:l] = v }
+  opt.on('-w') { |v| options[:w] = v }
+  opt.on('-c') { |v| options[:c] = v }
+  paths = opt.parse(ARGV)
+  [options, paths]
 end
 
-def count_stdin
-  inputs = $stdin.readlines.join
-  count = {
-    line: count_lines(inputs),
-    word: count_words(inputs),
-    byte: inputs.size
+def calculate_stdin
+  input = $stdin.readlines.join
+  property = {
+    line: count_lines(input),
+    word: count_words(input),
+    byte: input.size,
+    name: '',
+    directory: false
   }
-  [count]
+  [property]
 end
 
 def calculate_file_text(paths)
-  paths.map do |file_name|
-    property = { line: 0, word: 0, byte: 0, name: '', directory: false }
-    if FileTest.file?(file_name)
-      file_texts = File.read(file_name)
+  paths.map do |path|
+    property = { line: 0, word: 0, byte: 0 }
+    if FileTest.file?(path)
+      file_texts = File.read(path)
       property[:line] = count_lines(file_texts)
       property[:word] = count_words(file_texts)
       property[:byte] = file_texts.size
     end
-    property[:name] = file_name
-    property[:directory] = FileTest.directory?(file_name)
+    property[:name] = path
+    property[:directory] = FileTest.directory?(path)
     property
   end
 end
@@ -56,27 +58,27 @@ end
 def output(file_properties, options)
   is_only_files = file_properties.none? { |property| property[:directory] == true }
   is_only_files = file_properties[0].key?(:directory) if is_only_files
-  padding_left = is_only_files ? 5 : 8
+  width = is_only_files ? 5 : 8
   file_properties.each do |property|
     puts "wc: #{property[:name]}: Is a directory" if property[:directory]
-    output_by_options(property, options, padding_left)
+    output_by_options(property, options, width)
   end
   return if file_properties.size == 1
 
   totals = calculate_total(file_properties)
-  output_by_options(totals, options, padding_left)
+  output_by_options(totals, options, width)
 end
 
-def output_by_options(property, options, padding_left)
-  options = options.transform_values(&:!) unless options.value?(true)
-  print "#{property[:line]} ".rjust(padding_left) if options[:l]
-  print "#{property[:word]} ".rjust(padding_left) if options[:w]
-  print "#{property[:byte]} ".rjust(padding_left) if options[:c]
+def output_by_options(property, options, width)
+  no_options = !options.value?(true)
+  print "#{property[:line]} ".rjust(width) if options[:l] || no_options
+  print "#{property[:word]} ".rjust(width) if options[:w] || no_options
+  print "#{property[:byte]} ".rjust(width) if options[:c] || no_options
   puts property[:name]
 end
 
 def calculate_total(counts)
-  totals = { line: 0, word: 0, byte: 0, name: 'total' }
+  totals = { line: 0, word: 0, byte: 0, name: 'total', directory: false }
   counts.each do |property|
     totals[:line] += property[:line]
     totals[:word] += property[:word]
