a9c8851eb936882d1f4e7bd995ca88aa0b84c672
anada <anada@beingcorp.co.jp>
2024-03-21 12:02:42 +0900

    配列で取得するように

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -3,19 +3,14 @@
 require './shot'
 
 class Frame
-  attr_reader :first_shot, :second_shot, :third_shot
+  attr_reader :shots
 
-  def initialize(first_mark, second_mark = nil, third_mark = nil)
-    @first_shot = Shot.new(first_mark)
-    @second_shot = Shot.new(second_mark)
-    @third_shot = Shot.new(third_mark)
+  def initialize(shots)
+    @shots = shots.map { |shot| Shot.new(shot) }
   end
 
   def score
-    [
-      first_shot.score,
-      second_shot.score,
-      third_shot.score
-    ].sum
+    @shots.map(&:point).sum
+  end
   end
 end
