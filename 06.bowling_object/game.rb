88324f550fad24d3cafbcabbd77b6e1f734b7ae4
anada <anada@beingcorp.co.jp>
2024-03-21 12:06:28 +0900

    何フレーム目かを表すcountの命名変更

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -25,16 +25,16 @@ class Game
 
   def calc
     total_score = 0
-    @frames.each_with_index do |frame, count|
+    @frames.each_with_index do |frame, frame_count|
       total_score += frame.score
       next if frame.score != 10
-      next if count == 9
+      next if frame_count == 9
 
-      next_frame = @frames[count + 1]
-      total_score += if frame.shots[0].mark == 'X' && count == 8
+      next_frame = @frames[frame_count + 1]
+      total_score += if frame.shots[0].mark == 'X' && frame_count == 8
                        next_frame.shots[0].point + next_frame.shots[1].point
                      elsif frame.shots[0].mark == 'X'
-                       frame.strike(next_frame, @frames[count + 2].shots)
+                       frame.strike(next_frame, @frames[frame_count + 2].shots)
                      else
                        frame.spare(next_frame.shots)
                      end
