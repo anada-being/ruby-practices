8d3ba5138e48962c063049505e8ff4f576985158
anada <anada@beingcorp.co.jp>
2024-03-22 11:02:52 +0900

    フレームクラスにフレームごとのスコア計算をさせる

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -9,12 +9,26 @@ class Frame
     @shots = shots.map { |shot| Shot.new(shot) }
   end
 
-  def score
-    @shots.map(&:point).sum
+  def score(scores, frame_count)
+    frame_score = sum_frame(@shots)
+    return frame_score if frame_score != 10 || frame_count == 9
+
+    next_frame = scores[frame_count + 1]
+    frame_score += if @shots[0].strike? && frame_count == 8
+                     sum_frame(next_frame.shots[0,2])
+                   elsif @shots[0].strike?
+                     strike(next_frame.shots, scores[frame_count + 2].shots)
+                   else
+                     spare(next_frame.shots)
+                   end
   end
 
-  def strike(next_frame, after_next_shots)
-    next_frame.shots[0].strike? ? 10 + after_next_shots[0].point : next_frame.score
+  def sum_frame(shots)
+    shots.map(&:point).sum
+  end
+
+  def strike(next_shots, after_next_shots)
+    next_shots[0].strike? ? 10 + after_next_shots[0].point : sum_frame(next_shots)
   end
 
   def spare(next_shots)
--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -12,18 +12,7 @@ class Game
   def calc
     total_score = 0
     @frames.each_with_index do |frame, frame_count|
-      total_score += frame.score
-      next if frame.score != 10
-      next if frame_count == 9
-
-      next_frame = @frames[frame_count + 1]
-      total_score += if frame.shots[0].strike? && frame_count == 8
-                       next_frame.shots[0].point + next_frame.shots[1].point
-                     elsif frame.shots[0].strike?
-                       frame.strike(next_frame, @frames[frame_count + 2].shots)
-                     else
-                       frame.spare(next_frame.shots)
-                     end
+      total_score += frame.score(@frames, frame_count)
     end
     total_score
   end
