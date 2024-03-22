fe3056f38a1c8bd104d5d0d74f09ab75014bbbf2
anada <anada@beingcorp.co.jp>
2024-03-22 11:13:34 +0900

    privateメソッドにできるところはprivateメソッドにした。rubocop実施

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -14,14 +14,16 @@ class Frame
     return frame_score if frame_score != 10 || frame_count == 9
 
     next_frame = scores[frame_count + 1]
-    frame_score += if @shots[0].strike? && frame_count == 8
-                     sum_frame(next_frame.shots[0,2])
-                   elsif @shots[0].strike?
-                     strike(next_frame.shots, scores[frame_count + 2].shots)
-                   else
-                     spare(next_frame.shots)
-                   end
+    frame_score + if @shots[0].strike? && frame_count == 8
+                    sum_frame(next_frame.shots[0, 2])
+                  elsif @shots[0].strike?
+                    strike(next_frame.shots, scores[frame_count + 2].shots)
+                  else
+                    spare(next_frame.shots)
+                  end
   end
+
+  private
 
   def sum_frame(shots)
     shots.map(&:point).sum
