f4a13b3eb2be0037fbbb175664897c48501a73a9
anada <anada@beingcorp.co.jp>
2024-03-25 12:03:25 +0900

    Shotクラスのメソッドを使えるように修正

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -19,7 +19,7 @@ class Game
 
   def parse_frames(score)
     marks = score.split(',')
-    frames = (1..9).map { |i| marks[0] == 'X' ? Frame.new([marks.shift], i) : Frame.new(marks.shift(2), i) }
+    frames = (1..9).map { |i| Shot.new(marks[0]).strike? ? Frame.new([marks.shift], i) : Frame.new(marks.shift(2), i) }
     [*frames, Frame.new(marks, 10)]
   end
 end
