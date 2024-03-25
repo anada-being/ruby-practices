d3602830f8d54fc78b6daaf8155cf86543c8d9f9
anada <anada@beingcorp.co.jp>
2024-03-25 14:59:10 +0900

    命名変更

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -17,20 +17,20 @@ class Frame
 
     after_next_frame = @frame_count + 1
     bonus_score = if @is_strike
-                    @frame_count == 9 ? frames[@frame_count].shots[0, 2].sum(&:point) : strike(frames[@frame_count], frames[after_next_frame])
+                    @frame_count == 9 ? frames[@frame_count].shots[0, 2].sum(&:point) : calc_strike(frames[@frame_count], frames[after_next_frame])
                   else
-                    spare(frames[@frame_count].shots)
+                    calc_spare(frames[@frame_count].shots)
                   end
     frame_score + bonus_score
   end
 
   private
 
-  def strike(next_shots, after_next_shots)
+  def calc_strike(next_shots, after_next_shots)
     next_shots.is_strike ? 10 + after_next_shots.shots[0].point : next_shots.shots.sum(&:point)
   end
 
-  def spare(next_shots)
+  def calc_spare(next_shots)
     next_shots[0].point
   end
 end
