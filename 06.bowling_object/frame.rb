4772515bdcfea6c073033404a4df0561fb7f36e4
anada <anada@beingcorp.co.jp>
2024-03-27 09:19:57 +0900

    9フレーム目の判定をcalc_strikeで行う

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -17,7 +17,7 @@ class Frame
     next_frame = frames[@frame_number + 1]
     after_next_frame = frames[@frame_number + 2]
     bonus_score = if @shots[0].strike?
-                    @frame_number == 8 ? next_frame.shots[0, 2].sum(&:point) : calc_strike(next_frame, after_next_frame)
+                    calc_strike(next_frame, after_next_frame)
                   else
                     calc_spare(next_frame.shots)
                   end
@@ -27,6 +27,8 @@ class Frame
   private
 
   def calc_strike(next_frame, after_next_frame)
+    return next_frame.shots[0, 2].sum(&:point) if @frame_number == 8
+
     next_frame.shots[0].strike? ? 10 + after_next_frame.shots[0].point : next_frame.shots.sum(&:point)
   end
 
