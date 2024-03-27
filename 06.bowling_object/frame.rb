2c9ea8664530f8388c41b9c43c7a811f358bb534
anada <anada@beingcorp.co.jp>
2024-03-27 11:10:18 +0900

    is_strike、pointsでshotsを経由しない

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -3,23 +3,25 @@
 require './shot'
 
 class Frame
-  attr_reader :shots
+  attr_reader :shots, :is_strike, :points
 
   def initialize(shots, frame_number)
     @shots = shots
     @frame_number = frame_number
+    @is_strike = shots[0].strike?
+    @points = shots.map(&:point)
   end
 
   def score(frames)
-    frame_score = @shots.sum(&:point)
+    frame_score = @points.sum
     return frame_score if frame_score != 10 || @frame_number == 9
 
     next_frame = frames[@frame_number + 1]
     after_next_frame = frames[@frame_number + 2]
-    bonus_score = if @shots[0].strike?
+    bonus_score = if @is_strike
                     calc_strike(next_frame, after_next_frame)
                   else
-                    calc_spare(next_frame.shots)
+                    next_frame.points[0]
                   end
     frame_score + bonus_score
   end
@@ -27,12 +29,8 @@ class Frame
   private
 
   def calc_strike(next_frame, after_next_frame)
-    return next_frame.shots[0, 2].sum(&:point) if @frame_number == 8
+    return next_frame.points[0, 2].sum if @frame_number == 8
 
-    next_frame.shots[0].strike? ? 10 + after_next_frame.shots[0].point : next_frame.shots.sum(&:point)
-  end
-
-  def calc_spare(next_shots)
-    next_shots[0].point
+    next_frame.is_strike ? 10 + after_next_frame.points[0] : next_frame.points.sum
   end
 end
