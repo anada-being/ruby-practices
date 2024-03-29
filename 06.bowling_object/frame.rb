9d6e682ab64815e2736424f48e01a86ff479c880
anada <anada@beingcorp.co.jp>
2024-03-29 09:48:59 +0900

    エンドレスメソッド化、三項演算子の使用

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -3,27 +3,23 @@
 require './shot'
 
 class Frame
-  attr_reader :shots, :is_strike, :points
+  attr_reader :shots
 
   def initialize(shots, frame_number)
     @shots = shots
     @frame_number = frame_number
-    @is_strike = shots[0].strike?
-    @points = shots.map(&:point)
   end
 
+  def strike? = @shots[0].strike?
+  def points = @shots.map(&:point)
+
   def score(frames)
-    frame_score = @points.sum
-    return frame_score if frame_score != 10 || @frame_number == 9
+    return points.sum if points.sum != 10 || @frame_number == 9
 
     next_frame = frames[@frame_number + 1]
     after_next_frame = frames[@frame_number + 2]
-    bonus_score = if @is_strike
-                    calc_strike(next_frame, after_next_frame)
-                  else
-                    next_frame.points[0]
-                  end
-    frame_score + bonus_score
+    bonus_score = strike? ? calc_strike(next_frame, after_next_frame) : next_frame.points[0]
+    points.sum + bonus_score
   end
 
   private
@@ -31,6 +27,6 @@ class Frame
   def calc_strike(next_frame, after_next_frame)
     return next_frame.points[0, 2].sum if @frame_number == 8
 
-    next_frame.is_strike ? 10 + after_next_frame.points[0] : next_frame.points.sum
+    next_frame.strike? ? 10 + after_next_frame.points[0] : next_frame.points.sum
   end
 end
